b435ebe79765b4c62b52f3ac9bd6841f4fe8ea8b
Perihan Gire <56370086+PerihanGiree@users.noreply.github.com>
2023-09-08 18:59:08 +0300

    click-problem-update

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,6 @@ const Modal = ({ scorePlayer1, scorePlayer2 }) => {
   return (
     <div className="modal">
       <div className="modal-title">
-        {" "}
         Skor1:{scorePlayer1} Skor2:{scorePlayer2}
       </div>
       <div onClick={() => (window.location = "/")} className="modal-btn">
