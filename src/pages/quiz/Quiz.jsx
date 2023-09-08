c9c0378828db413fb15b8997280278f12984ebf7
Perihan Gire <56370086+PerihanGiree@users.noreply.github.com>
2023-09-08 18:55:00 +0300

    click-problem

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,3 +1,94 @@
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import QuestionCard from "../../components/questionCard/QuestionCard";
+// import Modal from "../../components/modal/Modal";
+// import LoadingIcons from "react-loading-icons";
+// import * as api from "../../api/api";
+
+// const Quiz = () => {
+//   const { difficulty } = useParams();
+//   const [questionsData, setQuestionsData] = useState([]);
+//   const [scorePlayer1, setScorePlayer1] = useState(0);
+//   const [scorePlayer2, setScorePlayer2] = useState(0);
+//   const [count, setCount] = useState(0);
+//   const [modal, setModal] = useState(false);
+//   const [loading, setLoading] = useState(true);
+//   const [activePlayer, setActivePlayer] = useState("player1");
+
+//   useEffect(() => {
+//     const getData = async () => {
+//       const data = await api.fetchQuizData(difficulty, 20);
+//       setQuestionsData(data);
+//       setLoading(false);
+//     };
+
+//     getData();
+//   }, [difficulty]);
+
+//   const switchPlayer = () => {
+//     setActivePlayer(activePlayer === "player1" ? "player2" : "player1");
+//   };
+
+//   const handleAnswer = (isCorrect) => {
+//     if (isCorrect) {
+//       if (activePlayer === "player1") {
+//         setScorePlayer1(scorePlayer1 + 100);
+//       } else if (activePlayer === "player2") {
+//         setScorePlayer2(scorePlayer2 + 100);
+//       }
+//     }
+
+//     setCount(count + 1);
+
+//     if (count === 9) {
+//       setModal(true);
+//     } else {
+//       switchPlayer();
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-row">
+//       <div className="flex flex-col bg-purple-600 w-full h-[100vh]">
+//         <div className="flex m-10 justify-center items-center">
+//           {loading ? (
+//             <div className="flex justify-center items-center mt-40">
+//               <LoadingIcons.Circles />
+//             </div>
+//           ) : (
+//             <div className="flex w-full h-full justify-center">
+//               {modal ? (
+//                 <Modal
+//                   scorePlayer1={scorePlayer1}
+//                   scorePlayer2={scorePlayer2}
+//                 />
+//               ) : (
+//                 <QuestionCard
+//                   title={
+//                     activePlayer === "player1" ? "OYUNCU BİR" : "OYUNCU İKİ"
+//                   }
+//                   questionsData={questionsData}
+//                   score={
+//                     activePlayer === "player1" ? scorePlayer1 : scorePlayer2
+//                   }
+//                   count={count}
+//                   setCount={setCount}
+//                   modal={modal}
+//                   handleAnswer={handleAnswer}
+//                   activePlayer={activePlayer}
+//                   switchPlayer={switchPlayer}
+//                 />
+//               )}
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Quiz;
+// Quiz.js
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import QuestionCard from "../../components/questionCard/QuestionCard";
@@ -33,7 +124,7 @@ const Quiz = () => {
     if (isCorrect) {
       if (activePlayer === "player1") {
         setScorePlayer1(scorePlayer1 + 100);
-      } else if (activePlayer === "player2") {
+      } else {
         setScorePlayer2(scorePlayer2 + 100);
       }
     }
